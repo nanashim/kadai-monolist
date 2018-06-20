6969810fb66de5a532dfc7f75da445735b8c6208
nana.shimizu <nana.shimizu@rakuten.com>
2018-06-20 07:16:31 +0000

    kadai

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -19,6 +19,19 @@
                                 @endif
                             </div>
                         </div>
+                        <!--have_button-->
+                        <div class="panel-body">
+                            @if ($item->id)
+                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @else
+                                <p class="item-title">{{ $item->name }}</p>
+                            @endif
+                            <div class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
